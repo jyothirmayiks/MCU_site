57a2876cd6cfadfdd811a14a8267e8550b96d0b4
jyothi <jothirmayiks@gmail.com>
2025-01-06 15:41:31 +0530

    feat: add js file

---
+++ script.js
@@ -0,0 +1,14 @@
+const searchInput = document.getElementById('searchInput');
+const characterCards = document.querySelectorAll('.character');
+
+searchInput.addEventListener('input', (event) => {
+  const searchValue = event.target.value.toLowerCase().trim(); 
+  characterCards.forEach(card => {
+    const name = card.querySelector('.name').textContent.trim(); 
+    if (name.toLowerCase().includes(searchValue)) {
+      card.style.display = 'block'; 
+    } else {
+      card.style.display = 'none'; 
+    }
+  });
+});
